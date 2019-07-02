0336f242bfcc67c1d34f6b30fc519e92ef713c36
jianrong.chen <jianrong.chen@luckincoffee.com>
2019-07-02 16:49:41 +0800

    添加githud用户信息配置文件

--- src/main/java/luckycoffee/jnoro/community/community/controller/AuthorizeController.java
+++ src/main/java/luckycoffee/jnoro/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import luckycoffee.jnoro.community.community.pojo.dto.AccessTokenDTO;
 import luckycoffee.jnoro.community.community.provider.GithubProvider;
 import luckycoffee.jnoro.community.community.provider.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,14 +19,26 @@ public class AuthorizeController {
     
     @Autowired
     private GithubProvider githubProvider;
+
+    /**
+     * 去配置文件里面读取配置的信息
+     */
+    @Value("${githud.client.id}")
+    private String clientId;
+
+    @Value("${githud.client.secret}")
+    private String clientSecret;
+
+    @Value("${githud.redirect.uri}")
+    private String redirectUri;
     
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("53f55e793eaa2935477a");
-        accessTokenDTO.setClient_secret("4d130d554f4bcaa47079ff27493924f20f251126");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
